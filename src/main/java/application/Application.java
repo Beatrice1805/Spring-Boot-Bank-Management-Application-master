049351e31e6fb27344f4ada93e46d437251ace55
Beatrice <oprea.beatrice@yahoo.com>
2020-05-24 19:21:40 +0300

    application.properties modifications

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,7 +2,6 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Created by Vlad on 20-Mar-17.
